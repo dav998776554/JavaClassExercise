541f746f70226f1bf243bd01c01e974eeb282157
Jihn <John@gmail.com>
2018-11-14 21:51:49 +0800

    Add FileLoad

---
+++ FileLoad/src/FileDemo.java
@@ -0,0 +1,11 @@
+import java.io.File;
+
+public class FileDemo {
+
+	public static void main(String[] args) {
+		FileUtil fu = new FileUtil();
+		fu.readFile("H6 October");
+
+	}
+
+}
---
+++ FileLoad/src/FileUtil.java
@@ -0,0 +1,20 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FileUtil {
+	public static String readFile(String Name) {
+		StringBuilder builder = new StringBuilder();
+		try {
+			Scanner sca = new Scanner(new FileInputStream(Name));
+			while(sca.hasNext()) {
+				builder.append(sca.nextLine());
+				builder.append('\n');
+			}
+		}catch(FileNotFoundException e) {
+			System.out.print("找不到指定檔案 請重新設定路徑或檔案名稱");
+		}
+		return builder.toString();
+	}
+}
