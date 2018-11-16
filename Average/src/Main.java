1c42abe8263809cca10481284aa1301c57e871bd
Jihn <John@gmail.com>
2018-11-16 21:29:37 +0800

    在Average資料夾內增加Main檔案用於測試使用AutoCloseable介面之關閉資源 Fix

---
+++ Average/src/Main.java
@@ -0,0 +1,38 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		try(Resourcee ree = new Resourcee();
+			Resource res = new Resource()) {
+			res.doSome();
+			ree.doOther();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
+
+
+class Resource implements AutoCloseable{
+	void doSome() {
+		System.out.println("一些資源");
+	}
+	@Override
+	public void close() throws Exception {
+		System.out.println("一些資源即將關閉");
+	}
+}
+
+class Resourcee implements AutoCloseable{
+	void doOther() {
+		System.out.println("其他資源");
+	}
+	
+	@Override
+	public void close() throws Exception {
+		System.out.println("其他資源即將關閉");
+		
+	}
+	
+}
